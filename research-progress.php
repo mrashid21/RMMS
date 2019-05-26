ebec371266e910cea185f3d1a6e14dc487655cd8
asnidajmlddn <asnidaj@yahoo.com>
2019-05-26 23:00:24 +0800

    Update research-progress.php

--- research-progress.php
+++ research-progress.php
@@ -62,7 +62,7 @@ if (isset($_POST['studentId'])) {
                             <a class="nav-link" href="overview.php"><i class="ni ni-tv-2 text-teal"></i>Overview</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" href="schedule.php"><i class="ni ni-calendar-grid-58 text-purple"></i>Schedule</a>
+                            <a class="nav-link" href="appointment.php"><i class="ni ni-calendar-grid-58 text-purple"></i>Appointment</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link" href="Checklist.php"><i class="ni ni-bullet-list-67 text text-green"></i>Checklist</a>
