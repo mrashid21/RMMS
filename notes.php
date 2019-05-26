ac401d650e8724df68325637f3dcc5da6fd898ae
Ayoob Mohammed Hassan <auoob_1990@yahoo.com>
2019-05-26 17:13:20 +0800

    .

--- notes.php
+++ notes.php
@@ -1,9 +1,9 @@
 <?php 
-// require "./assets/validation/validateUser.php";
-// require "./assets/helper/UserAction.php";
+require "./assets/validation/validateUser.php";
+require "./assets/helper/UserAction.php";
 
-// $img = UserAction::getImageDir();
-// $data = UserAction::retrieveData();
+$img = UserAction::getImageDir();
+$data = UserAction::retrieveData();
 ?>
 
 <!DOCTYPE html>
@@ -146,14 +146,14 @@
           <!-- <input type="submit" class="btn" value="Logout" style="width: 100%"> -->
           <div class="dropdown user user-menu" style="width:300px;">
             <a href="#" class="small-img" data-toggle="dropdown">
-              <!-- <img src=<?= $img ?> class="img-fluid img-circle header-user-image small-img" alt="User Image">
-              <span class="hidden-xs"><?= $_SESSION['logged_firstName'] . " " . $_SESSION['logged_lastName'] ?></span> -->
+              <img src=<?= $img ?> class="img-fluid img-circle header-user-image small-img" alt="User Image">
+              <span class="hidden-xs"><?= $_SESSION['logged_firstName'] . " " . $_SESSION['logged_lastName'] ?></span>
             </a>
             <ul class="dropdown-menu">
               <!-- Menu Body -->
               <li class="user-body p-4 header-profile-margin">
                 <div class="row d-flex justify-content-center">
-                  <img src=<?= $img ?> class="img-fluid img-circle card-user-image" alt="User Image">
+                  <img src=<?= $img ?> class = "img-fluid img-circle card-user-image" alt="User Image">
                 </div>
                 <div class="row d-flex justify-content-around">
                   <p class="text-center pt-2">
